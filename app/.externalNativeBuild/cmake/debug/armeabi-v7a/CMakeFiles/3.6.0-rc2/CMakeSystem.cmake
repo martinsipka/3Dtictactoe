d437a49a61566dc8f076e92029f2e6d928768e17
Martin Šípka <martinsipka@gmail.com>
2020-02-07 15:30:27 +0100

    Add build to remove it in next commit

--- app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.4.0-130-generic")
+set(CMAKE_HOST_SYSTEM "Linux-4.15.0-39-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.4.0-130-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-39-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/martin/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
